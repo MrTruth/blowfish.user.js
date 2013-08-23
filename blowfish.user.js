c2b630167db2ef3d436b3a2ad4d101d6c992ab74
MrTruth <131404@gmail.com>
2013-08-23 00:45:38 -0700

    Update blowfish.user.js

--- blowfish.user.js
+++ blowfish.user.js
@@ -2,6 +2,7 @@
 // @name Blowfish encryption 
 // @description This UserScript for encryption / decryption of text on a Web page. Usage: encode: select text, press ctrl+Q, enter password, press ok; decode: select text, press ctrl+shift+Q, enter password, press ok
 // @author github.com/MrTruth
+// @license MIT
 // @version 1.0.3
 // ==/UserScript==
 
